c93a719502eeb02f59e3f50492c96243863c01d8
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-10 16:01:54 +0200

    Step 2 and Step 3

--- hangman.py
+++ hangman.py
@@ -2,13 +2,29 @@
 
 import random
 '''STEP 1: GENERATE RANDOM WORD'''
+game_over = False
 word_list = ["ardvark", "baboon", "camel"]
 word = random.choice(word_list)
 
-prompt = input("Guess a letter: ").lower()
-for letter in word:
-    if prompt == letter: 
-        print("correct")
-    else: 
-        print("incorrect")
+'''STEP 2: GENERATE AS MANY BLANKS IN THE WORD'''
+# create empty list
+display = []    
 
+# for each letter in the word, add '-' 
+for letter in word:
+    display.append("-")
+print(display)
+
+while not game_over:
+    prompt = input("Guess a letter: ").lower()
+
+    for position in range(len(word)):
+        letter = word[position]
+        if prompt == letter: 
+            display[position] = letter
+    print(display)
+    '''STEP 3: CHECK IF PLAYER HAS WON'''
+    if "-" not in display:
+        game_over = True
+    
+print("You win")
